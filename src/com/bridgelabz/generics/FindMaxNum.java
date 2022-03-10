d3e318aa1fd4dc69310f2bfa0ca2cd60696b3e3f
sakshee07 <sakshee1@gmail.com>
2022-03-10 15:01:52 +0530

    added generics

---
+++ src/com/bridgelabz/generics/FindMaxNum.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.generics;
+
+import java.util.Scanner;
+
+public class FindMaxNum {
+	
+	public Float MaximumFloat(Float firstnumber,Float secondnumber,Float thirdnumber) {
+		Float maxFloat = firstnumber;
+		if (secondnumber.compareTo(maxFloat) > 0)
+			maxFloat = secondnumber;
+		if (thirdnumber.compareTo(maxFloat) > 0)
+			maxFloat = thirdnumber;
+		System.out.println("The Maximum Number is :"+maxFloat);
+		return maxFloat;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to program");
+		FindMaxNum obj = new FindMaxNum();
+		
+		Scanner sc =new Scanner(System.in);
+         System.out.println("Enter first number");
+        Float x = sc.nextFloat();
+         System.out.println("Enter second number");
+        Float y = sc.nextFloat();
+         System.out.println("Enter third number");
+        Float z = sc.nextFloat();
+         
+ 		 obj.MaximumFloat(x,y,z);
+
+	}
+}
